39baa40f04bfd703c390f75feb79c9763e457f97
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-17 12:10:59 +0500

    Create Card file to show components of card

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className="card">
+      <img src={require('../images/katie-zaferes.png')} alt="card-1"/>
+    </div>
+  )
+}

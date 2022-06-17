952dc3f5f03842a3dccb2592a301e362ebafbc91
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-17 11:59:11 +0500

    Create Hero section & give it a styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 import './index.css'
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
     
   )

998f4a25becbf2f88c940c62ab636d499e1b6dc7
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-17 12:30:57 +0500

    Create 1st card with hard code

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Card() {
   return (
     <div className="card">
-      <img src={require('../images/katie-zaferes.png')} alt="card-1"/>
+      <img src={require('../images/katie-zaferes.png')} alt="card-1" className="card--image" />
+      <div className="card--stats">
+        <img src={require('../images/star.png')} alt="rating" className="card--star"/>
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
+      </div>
+       <p>Life lessons with Katie Zaferes</p>
+       <p><span className="bold">From $136</span> / person</p>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav>
-      <img className="nav--logo" src={require('../images/airbnb-logo.png')} alt="Logo" />
+      <img className="nav--logo" src={require('../images/airbnb-logo.png')} alt="Logo"/>
     </nav>
   )
 }

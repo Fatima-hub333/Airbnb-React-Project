349f84b30cf22e98e0b7391b48dfd9fd9b8444d9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-19 00:16:41 +0500

    Loading images from .map()
    - change images folder place from src to public folder for working images in document

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     <div className="contacts">
       <Navbar />
       <Card
-        // img="katie-zaferes.png" not working 
+        img="katie-zaferes.png" 
         rating="5.0"
         reviewCount={6}
         country="USA"
@@ -21,31 +21,3 @@ export default function App() {
     
   )
 }
-
-// eslint-disable-next-line no-lone-blocks
-{/* <div className="contacts">
-            <Contact 
-                img="./images/mr-whiskerson.png" 
-                name="Mr. Whiskerson"
-                phone="(212) 555-1234"
-                email="mr.whiskaz@catnap.meow"
-            />
-            <Contact 
-                img="./images/fluffykins.png"
-                name="Fluffykins"
-                phone="(212) 555-2345"
-                email="fluff@me.com"
-            />
-            <Contact 
-                img="./images/felix.png"
-                name="Felix"
-                phone="(212) 555-4567"
-                email="thecat@hotmail.com"
-            />
-            <Contact 
-                img="./images/pumpkin.png"
-                name="Pumpkin"
-                phone="(0800) CAT KING"
-                email="pumpkin@scrimba.com"
-            />
-        </div> */}
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ export default function Card(props) {
   // console.log(props)
   return (
     <div className="card">
-      <img src={require('../images/katie-zaferes.png')} className="card--image" alt="card-1"/>
+      <img src={`../images/${props.img}`} className="card--image" alt=""card-1/>
       <div className="card--stats">
-        <img src={require('../images/star.png')} alt="rating" className="card--star"/>
+        <img src={'../images/star.png'} alt="rating" className="card--star"/>
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}) • </span>
         <span className="gray">{props.country}</span>

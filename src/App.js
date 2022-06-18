e4ecc9102f5552f9c63d63056a6ff7de77f2155b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-19 00:34:25 +0500

    Map experiences data into components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react'
 import Navbar from './components/Navbar'
 // import Hero from './components/Hero'
 import Card from './components/Card'
-// import Contact from './components/Contact'
+import data from './data'
 import './index.css'
 
 export default function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
   return (
     <div className="contacts">
       <Navbar />
-      <Card
-        img="katie-zaferes.png" 
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
     
   )
